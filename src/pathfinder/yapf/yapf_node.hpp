08b4004b42a79036220dd9d754238b305a57edcd
Michael Lutz <michi@icosahedron.de>
2010-12-23 00:14:55 +0100

    -Codechange: [YAPF] Move the key-dependant part of the node base into a new class so the node base can also be used to implement nodes not using Tile/Trackdir.

--- src/pathfinder/yapf/yapf_node.hpp
+++ src/pathfinder/yapf/yapf_node.hpp
@@ -54,9 +54,8 @@ struct CYapfNodeT {
 	int         m_cost;
 	int         m_estimate;
 
-	inline void Set(Node *parent, TileIndex tile, Trackdir td, bool is_choice)
+	inline void Set(Node *parent, bool is_choice)
 	{
-		m_key.Set(tile, td);
 		m_hash_next = NULL;
 		m_parent = parent;
 		m_cost = 0;
@@ -65,8 +64,6 @@ struct CYapfNodeT {
 
 	inline Node *GetHashNext() {return m_hash_next;}
 	inline void SetHashNext(Node *pNext) {m_hash_next = pNext;}
-	inline TileIndex GetTile() const {return m_key.m_tile;}
-	inline Trackdir GetTrackdir() const {return m_key.m_td;}
 	inline const Tkey_& GetKey() const {return m_key;}
 	inline int GetCost() const {return m_cost;}
 	inline int GetCostEstimate() const {return m_estimate;}
@@ -79,6 +76,23 @@ struct CYapfNodeT {
 		dmp.WriteLine("m_cost = %d", m_cost);
 		dmp.WriteLine("m_estimate = %d", m_estimate);
 	}
+};
+
+/** Yapf Node base for trackdir based specialisation. */
+template <class Tkey_, class Tnode>
+struct CYapfNodeTrackT : public CYapfNodeT<Tkey_, Tnode>
+{
+	typedef CYapfNodeT<Tkey_, Tnode> Base;
+	typedef Tnode                    Node;
+
+	inline void Set(Node *parent, TileIndex tile, Trackdir td, bool is_choice)
+	{
+		Base::Set(parent, is_choice);
+		this->m_key.Set(tile, td);
+	}
+
+	inline TileIndex GetTile() const { return this->m_key.m_tile; }
+	inline Trackdir GetTrackdir() const { return this->m_key.m_td; }
 };
 
 #endif /* YAPF_NODE_HPP */
